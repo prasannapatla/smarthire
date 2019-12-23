ab3ac1e0c47165dd2106b7cac3c125ba39c0f8b1
vkushal.shetty <vkushal.shetty@terralogic.com>
2019-12-24 00:23:04 +0530

    Updated Login Design

--- src/Vishdesign/VishdesignHtml.tsx
+++ src/Vishdesign/VishdesignHtml.tsx
@@ -11,14 +11,33 @@ var page = function (_this: any) {
                {/* Text space */}
                <div className="col-sm-6" >
                   <div className="col_container">
-                     
+
                   </div>
                </div>
                {/* login space */}
                <div className="col-sm-6" >
                   <div className="col_container">
-                     <div className="loginForm">
-
+                     <div className="rightTopContainer">
+                        <div className="loginForm">
+                           <svg width="350" height="520" className="svg_bg">
+                              <polygon className="svgForm" points="0,0 275,0 350,60 350,520 0,520 0,0"  />
+                              <polygon className="svgFormFold"  points="275,0 350,60 275,60" />
+                              Sorry, your browser does not support inline SVG.
+                           </svg>
+                           <div className="form">
+                              <div className="formDetails">
+                                 <h3 className="loginText">Login</h3>
+                                 <p className="detailsText">Enter your details to start.</p>
+                                 <p className="detailsText">E-mail</p>
+                                 <input type="text" placeholder="Email" id="lemail" required />
+                                 <p className="detailsText">Password</p>
+                                 <input type="password" placeholder="Password" id="lpwd" required />
+                                 <a className="forgot-pwd" style={{color:"#E7537F"}}>Forgot Password?</a><br /><br />
+                                 <input className="btn_style" type="button" onClick={_this.login_user.bind(_this)} value="Login" />
+                                 <div id="invalidprompt"></div>
+                              </div>
+                           </div>
+                        </div>
                      </div>
                   </div>
                </div>
