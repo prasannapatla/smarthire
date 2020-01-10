a09620ac3c4d542663a8856532f4f88cfb423890
terralogic2019 <brguru90@gmail.com>
2020-01-10 17:13:36 +0530

    Fri praj design

--- src/Admin_register/Admin_registerHtml.tsx
+++ src/Admin_register/Admin_registerHtml.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Admin_menu from '../Admin_menu/Admin_menu';
 import Banner from '../Banner/Banner';
+import { FiPlus } from 'react-icons/fi';
 
 var page = function (_this: any) {
     return (
@@ -11,32 +12,34 @@ var page = function (_this: any) {
                 <div className="row">
                     <Admin_menu />
                     <div className="col-sm-9 content">
-                        <Banner />
+                        {/* <Banner /> */}
                         <div className="data">
                             <div className="sect">
                                 <form style={{ fontSize: "15px;" }}>
                                     <div style={{ marginBottom: "10px" }}>
                                         <div className='heading'>Register Candidate</div>
+                                        <select id="cur_exam" className="cur_exam" style={{marginTop: "21px",marginBottom: "21px"}}></select>
+                                        <button className="register" onClick={_this.register_user.bind(_this)}><FiPlus/> Register</button>
                                         <input type="text" placeholder="Name" className="form-control rname" required />
                                         <input type="text" placeholder="Email" className="form-control remail" required />
-                                        <input type="number" placeholder="Mobile number" maxLength={10} className="form-control mob" />
-                                        <select id="cur_exam" className="cur_exam" style={{marginTop: "10px"}}></select>
-                                        <input type="button" onClick={_this.register_user.bind(_this)} value="Register" />
+                                        <input type="tel" placeholder="Mobile number" maxLength={10} className="form-control mob" required />
+                                        
                                     </div>
                                 </form>
                                 <div className='file_upload' style={{ display: "block" }}>
-                                <input type="file" className="upload_file"  />File type:xls,xlsx&nbsp;&nbsp;
-                                    <button  onClick={_this.bulk_upload.bind(_this)}>Upload file</button>
-                                    <b>Format: </b>Name,Email,Mobile number<br />
+                                    <b>Format: </b>Name,Email,Mobile number<br /><br />
+                                    <button className="upload" onClick={_this.bulk_upload.bind(_this)}>Upload</button>
+                                    <input type="file" className="upload_file" />
+                                    <br />
                                     <span className='progress'>-</span>
                                 </div>
                             </div>
-                            <div className="sect">
+                            <div className="credentials_sect">
                                 <form style={{ fontSize: "15px;" }}>
                                     <div style={{ marginBottom: "10px" }}>
                                         <div className='heading'>Send Credentials</div>
                                         <select id="cur_exam2" className="cur_exam2"></select>
-                                        <input type="button" onClick={_this.send_password.bind(_this)} value="Send User Credentials" />
+                                        <button className="send" onClick={_this.send_password.bind(_this)}><FiPlus/> Send</button>
                                     </div>
                                     <b>Failed emails: </b><br />
                                     <pre className='refresh_status' id='refresh_status' ></pre>
