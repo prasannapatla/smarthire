9a8be17c9748a3e2e8f65d7685f9c83a25ee8574
Guruprasad BR <guruprasad.br@terralogic.com>
2021-01-19 23:51:20 +0530

    .

--- smarthire/myenv/bin/activate.fish
+++ smarthire/myenv/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/media/guruprasad/Files/Office/workspace/smarthire-coding/smarthire/myenv'
+set -gx VIRTUAL_ENV '/var/www/smarthire/smarthire/myenv'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
