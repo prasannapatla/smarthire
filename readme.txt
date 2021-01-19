b37f1cf1bbfaa309547197ca11d495739df21279
Guruprasad BR <gubr@cisco.com>
2021-01-19 22:11:13 +0530

    fixed untracked files

--- readme.txt
+++
@@ -1,7 +0,0 @@
-pip install git+https://github.com/PyMySQL/mysqlclient-python.git
-
-
-
-Default Super admin credentials will be
-Email ID: admin@terralogic.com
-Password: terralogic
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo fuser -k 3000/tcp 7000/tcp
-clear
-. ./smarthire/myenv/bin/activate
-python3 smarthire/manage.py makemigrations
-python3 smarthire/manage.py migrate
-python3 smarthire/manage.py runserver 7000
